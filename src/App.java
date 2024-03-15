4465ca2bd8e37d0a89716dca0a7bbd052ce4144a
Mohamed Tarek <mt444458@gmail.com>
2024-03-15 23:57:27 +0200

    problem solved

--- src/App.java
+++ src/App.java
@@ -1,19 +1,34 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        displayArray(numbers);
 
-        int index = 0;
+        int index_of_current_odd_number = 0;
         int swapper = 0;
-        for (int i = index; i < numbers.length; i++) {
+
+        for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] % 2 != 0) {
-                index = i;
+                if (swapper != 0) {
+                    continue;
+                }
+                index_of_current_odd_number = i;
                 swapper = numbers[i];
-            } else {
-                swapper = numbers[i];
-                numbers[i] = numbers[index];
-                numbers[index] = swapper;
-                index = i;
+                continue;
             }
+            numbers[index_of_current_odd_number] = numbers[i];
+            numbers[i] = swapper;
+            swapper = 0;
+            i = index_of_current_odd_number;
         }
+        displayArray(numbers);
+    }
+
+    public static void displayArray(int[] arr) {
+        int length = arr.length;
+        System.out.print("\n{ ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(i != length - 1 ? arr[i] + ", " : arr[i] + " ");
+        }
+        System.out.print("}\n");
     }
 }
