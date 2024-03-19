00952fbb6a9cbe2e76b9b8a1cacc3cbd7fc532cb
Mohamed Tarek <mt444458@gmail.com>
2024-03-19 18:02:41 +0200

    add test package

--- src/problem_set_2/FootballMatch.java
+++ src/problem_set_2/FootballMatch.java
@@ -1,7 +1,7 @@
 package problem_set_2;
 
 public class FootballMatch {
-    private byte original_time;
+    private int winner;
     private FootballTeam team1;
     private FootballTeam team2;
     
--- src/problem_set_2/FootballTeam.java
+++ src/problem_set_2/FootballTeam.java
@@ -1,8 +1,18 @@
 package problem_set_2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FootballTeam {
-    private String name;
-    private Player[] players;
+    private String team_name;
+    private Player[] team_players = new Player[5];
+    
+    public FootballTeam(String team_name, Player[] team_players) {
+        this.team_name = team_name;
+        this.team_players = team_players;
+    }
+
+    @Override
+    public String toString() {
+        return "FootballTeam [team_name=" + team_name + ", team_players=" + Arrays.toString(team_players) + "]";
+    }
 }
