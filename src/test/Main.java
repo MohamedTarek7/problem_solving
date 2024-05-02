403695206e20a94252fc80a7c55a9ec5dbf9da40
Mohamed Tarek <mt444458@gmail.com>
2024-05-02 22:31:29 +0300

    new problem

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,5 +1,39 @@
 package test;
 
-public class Main {         
-    
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        display_array_elements(even_numbers(arr));
+    }
+
+    public static void display_array_elements(int[] arr) {
+        System.out.print("{ ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.print("}");
+    }
+
+    public static int[] even_numbers(int start, int end) {
+
+        int[] result;
+
+        for (int i = start; i < end; i++) {
+            if (i % 2 == 0) {
+                result.
+            }
+        }
+
+        int index = 0;
+
+        for (int i : arr) {
+            if (i % 2 == 0) {
+                result[index] = i;
+                index++;
+            }
+        }
+
+        return result;
+    }
 }
